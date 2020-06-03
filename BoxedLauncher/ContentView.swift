774ddead08c63c9caf1015d961fff686ca1e1f1e
Ankith Madadi <ankithm28@gmail.com>
2020-06-03 12:10:00 -0700

    Init Again

---
+++ BoxedLauncher/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BoxedLauncher
+//
+//  Created by Ankith on 6/3/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
