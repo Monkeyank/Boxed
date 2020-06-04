5584d82bdb260e1cce75594f013750b8e8ef295d
Ankith Madadi <ankithm28@gmail.com>
2020-06-04 15:34:50 -0700

    add shortcut + window manager files

---
+++ Boxed/ShortcutManager.swift
@@ -0,0 +1,14 @@
+//
+//  ShortcutManager.swift
+//  Boxed
+//
+//  Created by Ankith on 6/4/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Foundation
+import MASShortcut
+
+class ShortcutManager {
+    
+}
---
+++ Boxed/WindowManager.swift
@@ -0,0 +1,13 @@
+//
+//  WindowManager.swift
+//  Boxed
+//
+//  Created by Ankith on 6/4/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Cocoa
+
+class WindowManager {
+    
+}
