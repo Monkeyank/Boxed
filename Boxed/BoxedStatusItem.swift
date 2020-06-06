282d9bc9361db3c0d747d09c55e14f774a3e70a8
Ankith Madadi <ankithm28@gmail.com>
2020-06-06 01:06:06 -0700

    Boxed Status Item

---
+++ Boxed/BoxedStatusItem.swift
@@ -0,0 +1,51 @@
+//
+//  BoxedStatusItem.swift
+//  Boxed
+//
+//  Created by Ankith on 6/6/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Cocoa
+
+class BoxedStatusItem {
+    static let instance = BoxedStatusItem()
+    private var added: Bool = false
+    private var nsStatusItem: NSStatusItem?
+    public var statusMenu: NSMenu? {
+        didSet {
+            nsStatusItem?.menu = statusMenu
+        }
+    }
+    
+    private init() {}
+    
+    public func refreshVisibility() {
+        if Defaults.hideMenuBarIcon.enabled {
+            remove()
+        } else {
+            add()
+        }
+    }
+    
+    public func openMenu() {
+        if !added {
+            add()
+        }
+        nsStatusItem?.button?.performClick(self)
+        refreshVisibility()
+    }
+    
+    private func add() {
+        added = true
+        nsStatusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        nsStatusItem?.menu = self.statusMenu
+        nsStatusItem?.button?.image = NSImage(named: "StatusTemplate")
+    }
+    
+    private func remove() {
+        added = false
+        guard let nsStatusItem = nsStatusItem else { return }
+        NSStatusBar.system.removeStatusItem(nsStatusItem)
+    }
+}
