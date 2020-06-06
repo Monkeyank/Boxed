aac6ebab7c74f5d947b03208b9065909e43512db
Ankith Madadi <ankithm28@gmail.com>
2020-06-06 01:33:24 -0700

    Accessibility Init

---
+++ Boxed/Accessibility/AccessibilityAuthorization.swift
@@ -0,0 +1,9 @@
+//
+//  AccessibilityAuthorization.swift
+//  Boxed
+//
+//  Created by Ankith on 6/6/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Foundation
---
+++ Boxed/Accessibility/AcessibilityWindowController.swift
@@ -0,0 +1,9 @@
+//
+//  AcessibilityWindowController.swift
+//  Boxed
+//
+//  Created by Ankith on 6/6/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Foundation
