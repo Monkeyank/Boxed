6639930ba0639f307c3dad9285e45c742d3c3e00
Ankith Madadi <ankithm28@gmail.com>
2020-06-06 15:22:23 -0700

    Accessibility Folder

--- Boxed/Accessibility/AccessibilityAuthorization.swift
+++ Boxed/Accessibility/AccessibilityAuthorization.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Cocoa
+
+class AcessibilityAuthorization {
+    private var accessibilityWindowController: NSWindowController?
+    
+    public func checkAccessibility(completion: @escaping () -> Void) -> Bool {
+        if !AXIsProcessTrusted() {
+            accessibilityWindowController = NSStoryboard(name: "Main", bundle: nil).instantiateController(withIdentifier: "AccessibiilityWindowController") as? NSWindowController
+            NSApp.activate(ignoringOtherApps: true)
+            accessibilityWindowController?.showWindow(self)
+            accessibilityWindowController?.window?.makeKey()
+            pollAccessibility(completion: completion)
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    private func pollAccessibility(completion: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            if AXIsProcessTrusted() {
+                self.accessibilityWindowController?.close()
+                self.accessibilityWindowController = nil
+                completion()
+            } else {
+                self.pollAccessibility( completion: completion )
+            }
+        }
+    }
+    
+    func showAuthorizationWindow() {
+        if accessibilityWindowController?.window?.isMiniaturized == true {
+                accessibilityWindowController?.window?.deminiaturize(self)
+            }
+            NSApp.activate(ignoringOtherApps: true)
+        }
+}
--- Boxed/Accessibility/AcessibilityWindowController.swift
+++ Boxed/Accessibility/AcessibilityWindowController.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Ankith. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class AccessibilityWindowController: NSWindowController {
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        let closeButton = self.window?.standardWindowButton(.closeButton)
+        closeButton?.target = self
+        closeButton?.action = #selector(quit)
+    }
+    
+    @objc func quit() {
+        exit(1)
+    }
+}
+
