31c87d88ee8a338fd3d654cf8a16f294e3c33c0c
Ankith Madadi <ankithm28@gmail.com>
2020-06-06 18:24:31 -0700

    Boilerplate funcs for main part of ScreenDetection

--- Boxed/ScreenDetection.swift
+++ Boxed/ScreenDetection.swift
@@ -10,6 +10,39 @@ import Cocoa
 
 class ScreenDetection {
     
+    func detectScreens(using frontmostWindowElement: AccessibilityElement?) -> UsableScreens? {
+        let screens = NSScreen.screens
+        guard let firstScreen = screens.first else { return nil }
+        
+        if (screens.count == 1) {
+            let adjacentScreens = Defaults.traverseSingleScreen.enabled == true
+            ? AdjacentScreens(prev: firstScreen, next: firstScreen)
+            : nil
+            
+            return UsableScreens(currentScreen: firstScreen, adjacentScreens: adjacentScreens, numScreens: screens.count)
+        }
+    }
+    
+    struct UsableScreens {
+        let currentScreen: NSScreen
+        let adjacentScreens: AdjacentScreens?
+        let frameOfCurrentScreen: CGRect
+        let visibleFrameOfCurrentScreen: CGRect
+        let numScreens: Int
+        
+        init(currentScreen: NSScreen, adjacentScreens: AdjacentScreens? = nil, numScreens: Int) {
+            self.currentScreen = currentScreen
+            self.adjacentScreens = adjacentScreens
+            self.frameOfCurrentScreen = NSRectToCGRect(currentScreen.frame)
+            self.visibleFrameOfCurrentScreen =  NSRectToCGRect(currentScreen.visibleFrame)
+            self.numScreens = numScreens
+        }
+    }
+
+    struct AdjacentScreens {
+        let prev: NSScreen
+        let next: NSScreen
+    }
 }
 
 struct UsableScreens {
