4b7bf616b3592a7ebe170a4c63796eef595c46ea
Ankith Madadi <ankithm28@gmail.com>
2020-06-09 23:23:30 -0700

    Finish up repeated exect calculation

--- Boxed/WindowCalculations/RepeatedExecutionsCalculation.swift
+++ Boxed/WindowCalculations/RepeatedExecutionsCalculation.swift
@@ -9,9 +9,32 @@
 import Foundation
 
 protocol RepeatedExecutionsCalculation {
+    func calculateFirstRect(_ window: Window, lastAction: BoxedAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult
     
+    func calculateSecondRect(_ window: Window, lastAction: BoxedAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult
+
+    func calculateThirdRect(_ window: Window, lastAction: BoxedAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult
 }
 
 extension RepeatedExecutionsCalculation {
-    
+    func calculateRepeatedRect(_ window: Window, lastAction: BoxedAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult {
+        
+        guard let count = lastAction?.count,
+            lastAction?.action == action
+        else {
+            return calculateFirstRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        }
+                
+        let position = count % 3
+        
+        switch (position) {
+        case 1:
+            return calculateSecondRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        case 2:
+            return calculateThirdRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        default:
+            return calculateFirstRect(window, lastAction: lastAction, visibleFrameOfScreen: visibleFrameOfScreen, action: action)
+        }
+        
+    }
 }
