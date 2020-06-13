f2d459bf549234879b5c49b419fd259f6fc22a2d
Ankith Madadi <ankithm28@gmail.com>
2020-06-13 00:12:05 -0700

    Center Calculation

---
+++ Boxed/WindowCalculations/CenterCalculation.swift
@@ -0,0 +1,25 @@
+//
+//  CenterCalculation.swift
+//  Boxed
+//
+//  Created by Ankith on 6/13/20.
+//  Copyright © 2020 Ankith. All rights reserved.
+//
+
+import Foundation
+
+class CenterCalculation: WindowCalculation {
+    override func calculateRect(_ window: Window, lastAction: BoxedAction?, visibleFrameOfScreen: CGRect, action: WindowAction) -> RectResult {
+
+        if rectFitsWithinRect(rect1: window.rect, rect2: visibleFrameOfScreen) {
+            var calculatedWindowRect = window.rect
+            calculatedWindowRect.origin.x = round((visibleFrameOfScreen.width - window.rect.width) / 2.0) + visibleFrameOfScreen.minX
+            calculatedWindowRect.origin.y = round((visibleFrameOfScreen.height - window.rect.height) / 2.0) + visibleFrameOfScreen.minY
+            return RectResult(calculatedWindowRect)
+        } else {
+            return RectResult(visibleFrameOfScreen)
+        }
+
+    }
+    
+}
